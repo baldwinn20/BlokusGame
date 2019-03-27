604c979d4bc33d05f14f816669f9ad90245c0981
pascuad21 <pascuad21@up.edu>
2019-03-26 18:14:47 -0700

    created the default board

--- app/src/main/java/com/example/nicholasbaldwin/mockupgui/BlokusBoard.java
+++ app/src/main/java/com/example/nicholasbaldwin/mockupgui/BlokusBoard.java
@@ -25,23 +25,67 @@ private final float TILE_TOTAL_PERCENTAGE = TILE_SIZE_PERCENT
  */
 
 protected BlokusGameState state; // the current games state
+    //the offset from the left and top to the beginning of the board
     protected float hBase;
     protected float vBase;
-
+protected float fullSquare; // the size of the surfaceView
 
 
     public BlokusBoard(Context context) {
         super(context);
+        init();
+    }
+
+    private void init(){setBackgroundColor(Color.DKGRAY);}
+
+    private void updateDimensions(Canvas canvas){
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+        /*sets the full square to the smallest value between
+        the height and the width of the surface view
+         */
+
+        if(width> height){
+            fullSquare = height;
+            vBase = 0;
+            hBase = (width - height)/(float) 2.0;
+        }
+        else{
+            fullSquare = width;
+            hBase = 0;
+            vBase = (height - width)/ (float) 2.0;
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas){
         Paint bgColor = new Paint();
         bgColor.setColor(Color.GRAY);
+        //paints the horizontal and vertical lines
+        for(int i = 0; i < BOARD_LENGTH; i++){
+            float left = TILE_SIZE_PERCENT + (i * TILE_TOTAL_PERCENTAGE);
+            float right = left + DIVIDER_PRCENTAGE;
+            float top = 0;
+            float bottom = 100;
+            canvas.drawRect(hLocation(left),vLocation(top), hLocation(right)
+                    , vLocation(bottom), bgColor);
+            //the inverse to draw the vertical lines
+            canvas.drawRect(hLocation(top),vLocation(left), hLocation(bottom)
+                    , vLocation(right), bgColor);
+        }
 
-
-
-
+    }
+    /*
+    Helper methods to convert the percentages to both horizontal
+    pixel location (hLocation) and vertical pixel location
+    (vLocation)
+     */
+    private float hLocation(float percent){
+        return hBase + percent * fullSquare / 100;
+    }
+    private float vLocation(float percent){
+        return vBase + percent * fullSquare / 100;
     }
 
 
