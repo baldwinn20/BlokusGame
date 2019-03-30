e28f5c336290f13fbf829124cdce6509934eb596
pascuad21 <pascuad21@up.edu>
2019-03-30 15:41:26 -0700

    Relocated the pieces array from blokus gamestate to the piece class

--- app/src/main/java/com/example/nicholasbaldwin/mockupgui/Piece.java
+++ app/src/main/java/com/example/nicholasbaldwin/mockupgui/Piece.java
@@ -24,6 +24,9 @@ public class Piece {
     //0,1,2,3 are different orientation displays
     protected int orientationVal = 0;
 
+    //how the pieces are arranged in terms of ints
+    protected int[][] pieceLayout = new int[7][7];
+
     //if true, remove from a player's inventory user interface
     protected boolean isOnBoard = false;
     //An array of this width will be used to contain each piece
@@ -44,6 +47,147 @@ public class Piece {
         pieceValue = initScore;
         pieceColor = initColor;
         orientationVal = 0;
+
+        //setting up a blank piece layout
+        for(int i = 0; i < pieceLayout.length; i++){
+            for(int j = 0; j < pieceLayout.length;j++){
+                pieceLayout[i][j] = -1;
+            }
+        }
+
+        //the setups for each piece
+        if(pieceName.equals("one")){
+          pieceLayout[3][3] = 0;
+        }
+        else if(pieceName.equals("two")){
+            pieceLayout[3][2] = 0;
+            pieceLayout[3][3] = 0;
+        }
+        else if(pieceName.equals("VThree")){
+            pieceLayout[2][3] = 0;
+            pieceLayout[3][2] = 0;
+            pieceLayout[3][3] = 0;
+        }
+        else if(pieceName.equals("IThree")){
+            pieceLayout[3][2] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[3][4] = 0;
+        }
+        else if(pieceName.equals("TFour")){
+            pieceLayout[3][2] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[2][3] = 0;
+            pieceLayout[4][3] = 0;
+        }
+        else if(pieceName.equals("O")){
+            pieceLayout[2][3] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[2][2] = 0;
+            pieceLayout[3][2] = 0;
+        }
+        else if(pieceName.equals("LFour")){
+            pieceLayout[2][2] = 0;
+            pieceLayout[2][3] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[4][3] = 0;
+        }
+        else if(pieceName.equals("IFour")){
+            pieceLayout[3][2] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[3][4] = 0;
+            pieceLayout[3][5] = 0;
+        }
+        else if(pieceName.equals("ZFour")){
+            pieceLayout[2][2] = 0;
+            pieceLayout[2][3] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[3][4] = 0;
+        }
+        else if(pieceName.equals("F")){
+            pieceLayout[3][2] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[2][3] = 0;
+            pieceLayout[2][4] = 0;
+            pieceLayout[4][3] = 0;
+        }
+        else if(pieceName.equals("X")){
+            pieceLayout[3][2] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[3][4] = 0;
+            pieceLayout[2][3] = 0;
+            pieceLayout[4][3] = 0;
+        }
+        else if(pieceName.equals("P")){
+            pieceLayout[3][2] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[3][4] = 0;
+            pieceLayout[4][2] = 0;
+            pieceLayout[4][3] = 0;
+        }
+        else if(pieceName.equals("W")){
+            pieceLayout[2][3] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[2][2] = 0;
+            pieceLayout[3][4] = 0;
+            pieceLayout[4][4] = 0;
+        }
+        else if(pieceName.equals("ZFive")){
+            pieceLayout[2][2] = 0;
+            pieceLayout[2][3] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[4][3] = 0;
+            pieceLayout[4][4] = 0;
+        }
+        else if(pieceName.equals("Y")){
+            pieceLayout[1][3] = 0;
+            pieceLayout[2][3] = 0;
+            pieceLayout[2][4] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[4][3] = 0;
+        }
+        else if(pieceName.equals("LFive")){
+            pieceLayout[1][3] = 0;
+            pieceLayout[1][4] = 0;
+            pieceLayout[2][3] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[4][3] = 0;
+        }
+        else if(pieceName.equals("U")){
+            pieceLayout[2][4] = 0;
+            pieceLayout[2][3] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[4][3] = 0;
+            pieceLayout[4][4] = 0;
+        }
+        else if(pieceName.equals("TFive")){
+            pieceLayout[3][2] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[3][4] = 0;
+            pieceLayout[2][4] = 0;
+            pieceLayout[4][4] = 0;
+        }
+        else if(pieceName.equals("VFive")){
+            pieceLayout[2][2] = 0;
+            pieceLayout[2][3] = 0;
+            pieceLayout[2][4] = 0;
+            pieceLayout[3][4] = 0;
+            pieceLayout[4][4] = 0;
+        }
+        else if(pieceName.equals("N")){
+            pieceLayout[3][2] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[2][3] = 0;
+            pieceLayout[4][2] = 0;
+            pieceLayout[5][2] = 0;
+        }
+        else if(pieceName.equals("IFive")){
+            pieceLayout[3][1] = 0;
+            pieceLayout[3][2] = 0;
+            pieceLayout[3][3] = 0;
+            pieceLayout[3][4] = 0;
+            pieceLayout[3][5] = 0;
+        }
+
     }
 
     //Setters and Getters
