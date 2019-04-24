fa99bae2a828596460a195d3d30ce3b5f6f84669
baldwinn20 <42782171+baldwinn20@users.noreply.github.com>
2019-04-23 23:07:46 -0700

    Commented HelpMenu and QuitMenu classes

--- app/src/main/java/com/example/nicholasbaldwin/mockupgui/HelpMenu.java
+++ app/src/main/java/com/example/nicholasbaldwin/mockupgui/HelpMenu.java
@@ -11,7 +11,8 @@ import java.io.Serializable;
  * <!-- class HelpMenu-->
  * <p>
  * This class implements a popup menu when the help button
- * is pushed
+ * is pushed. It displays the game rules and has a button that
+ * allows the user to return to their game.
  *
  * @author <Justin Cao, Dylan Pascua, Nicholas Baldwin>
  * @version <Spring 2019>
@@ -29,6 +30,7 @@ public class HelpMenu extends AppCompatActivity implements Serializable {
         setContentView(R.layout.help_menu);
         returnButton = findViewById(R.id.exitButton);
 
+        //closes activity when "return" button is touched
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
